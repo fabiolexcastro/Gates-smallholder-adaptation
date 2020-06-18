e67be4848c8f850d99109fd6d342b1530a17483a
jramirezv <dawnpatrolmustaine@gmail.com>
2020-06-18 09:38:22 +0000

    update ERA scripts

--- adaptation/.ipynb_checkpoints/ERA_analyses-checkpoint.R
+++ adaptation/.ipynb_checkpoints/ERA_analyses-checkpoint.R
@@ -7,7 +7,7 @@ soldir <- paste(wd,"/soilgrids_global/5km",sep="")
 hzdir <- paste(wd,"/hazard_layers",sep="")
 
 #analysis version
-vr <- 2
+vr <- 3
 
 #Practice (n) as follows
 #   1. Reduced Tillage (47)
@@ -15,7 +15,7 @@ vr <- 2
 #   3. Agroforestry Pruning (38)
 #   4. Green Manure (37)
 #   5. Water Harvesting (35)
-prname <- "Mulch"
+prname <- "Water Harvesting"
 
 #load library
 library(analogues)
@@ -81,12 +81,28 @@ if (!file.exists(paste(cimdir,"/soils.rda",sep=""))) {
     load(paste(cimdir,"/soils.rda",sep=""))
 }
 
-#monthly hazard data
-hzrs_m <- stack(paste(hzdir,"/dry_days_hist/monthly/dry_days_1981_2019_",1:12,"_mean.asc",sep="")) %>%
+#monthly dry days hazard data
+hzrs_m <- stack(paste(hzdir,"/dry_days_hist/monthly/dry_days_1981_2019_",1:12,"_mean.tif",sep="")) %>%
             raster::readAll(.) %>%
             raster::crop(., msk) %>%
             raster::resample(., msk)
-hzrs_v <- stack(paste(hzdir,"/dry_days_hist/monthly/dry_days_1981_2019_",1:12,"_cv.asc",sep="")) %>%
+hzrs_v <- stack(paste(hzdir,"/dry_days_hist/monthly/dry_days_1981_2019_",1:12,"_cv.tif",sep="")) %>%
+            raster::readAll(.) %>%
+            raster::crop(., msk) %>%
+            raster::resample(., msk)
+
+#ppt driest quarter data
+pdq_rs <- raster(paste(hzdir,"/ppt_driest_quarter/pdq_hist.tif",sep="")) %>%
+            raster::readAll(.) %>%
+            raster::crop(., msk)
+
+#aridity index data
+ai_rs <- raster(paste(hzdir,"/aridity/aridity_thornthwaite_hist.tif",sep="")) %>%
+            raster::readAll(.) %>%
+            raster::crop(., msk)
+
+#chirps cv dta
+chcv_rs <- raster(paste(hzdir,"/chirps_cv/cvr_africa.tif",sep="")) %>%
             raster::readAll(.) %>%
             raster::crop(., msk) %>%
             raster::resample(., msk)
@@ -144,7 +160,7 @@ pdata_n <- pdata[which(pdata$RR < 0.15),]
 pdata_n$ID <- paste("s",1:nrow(pdata_n),sep="")
 
 #output directory
-pr_odir <- paste(cimdir,"/",gsub(" ","_",prname,fixed=T),"_v",vr,sep="")
+pr_odir <- paste(cimdir,"/",gsub(" ", "_", prname, fixed=T),"_v",vr,sep="")
 if (!file.exists(pr_odir)) {dir.create(pr_odir,recursive=T)}
 
 #plot
@@ -168,13 +184,14 @@ load(paste(cimdir,"/wc_tmean.rda",sep="")); wc_tmean <- stack(wc_tmean)
 #3. similarity for hazard layer (mean and var)
 #4. combine all in a sensible layer and save individual layers and output as .RData
 run_points <- function(in_data, etype='pos') {
+    allstk <- c()
     for (i in 1:nrow(in_data)) {
       #i <- 1
       cat("processing site=",i,"\n")
       if (!file.exists(paste(pr_odir,"/out_",etype,"_",in_data$ID[i],".RData",sep=""))) {
           #1. mean climate
           cat("...mean climate\n")
-          par1 <- createParameters(x=in_data$Lon[i], y=in_data$Lat[i], vars=c("prec","tmean"),weights=c(0.50,0.50),
+          par1 <- createParameters(x=in_data$Lon[i], y=in_data$Lat[i], vars=c("prec","tmean"),weights=c(0.75,0.25),
                                    ndivisions=c(12,12),growing.season=c(1,12),
                                    rotation="prec",threshold=1,env.data.ref=list(wc_prec,wc_tmean), 
                                    env.data.targ=list(wc_prec,wc_tmean),outfile=pr_odir,
@@ -194,32 +211,72 @@ run_points <- function(in_data, etype='pos') {
                                        fname=NA,writefile=F)
               simsol <- c(simsol,calc_similarity(parx))
           }
-          sim2 <- stack(simsol) %>%
-                  calc(., fun=mean, na.rm=T)
-          if (!inMemory(sim2)) {sim2 <- readAll(sim2)}
+          sim2 <- stack(simsol) %>% min(., na.rm=TRUE)
+          if (!inMemory(sim2)) {sim2 <- readAll(sim2)} 
 
           #3. similarity hazard layer (mean and variability)
+          cat("...hazard layers\n")
           #a. mean
+          cat("   ...mean\n")
           par3a <- createParameters(x=in_data$Lon[i], y=in_data$Lat[i], vars=c("tmean"),weights=1,
                                    ndivisions=c(12),growing.season=c(1,12),
                                    rotation="tmean",threshold=1,env.data.ref=list(stack(hzrs_m)), 
                                    env.data.targ=list(stack(hzrs_m)),outfile=pr_odir,
                                    fname=NA,writefile=F)
           sim3a <- calc_similarity(par3a)
+          if (!inMemory(sim3a)) {sim3a <- readAll(sim3a)}
 
           #b. variability
+          cat("   ...variability\n")
           par3b <- createParameters(x=in_data$Lon[i], y=in_data$Lat[i], vars=c("tmean"),weights=1,
                                    ndivisions=c(12),growing.season=c(1,12),
                                    rotation="tmean",threshold=1,env.data.ref=list(stack(hzrs_v)), 
                                    env.data.targ=list(stack(hzrs_v)),outfile=pr_odir,
                                    fname=NA,writefile=F)
           sim3b <- calc_similarity(par3b)
-          sim3 <- (sim3a + sim3b) / 2
+          if (!inMemory(sim3b)) {sim3b <- readAll(sim3b)}
+          
+          #c. ppt driest quarter
+          cat("   ...prec driest quarter\n")
+          par3c <- createParameters(x=in_data$Lon[i], y=in_data$Lat[i], vars=c("bio_1"),
+                                    weights=c(1),ndivisions=c(1),growing.season=NA,
+                                    rotation="none",threshold=1,env.data.ref=list(pdq_rs), 
+                                    env.data.targ=list(pdq_rs),outfile=pr_odir,
+                                    fname=NA,writefile=F)
+          sim3c <- calc_similarity(par3c)
+          if (!inMemory(sim3c)) {sim3c <- readAll(sim3c)}
+          
+          #d. aridity index
+          cat("   ...aridity index\n")
+          par3d <- createParameters(x=in_data$Lon[i], y=in_data$Lat[i], vars=c("bio_1"),
+                                    weights=c(1),ndivisions=c(1),growing.season=NA,
+                                    rotation="none",threshold=1,env.data.ref=list(ai_rs), 
+                                    env.data.targ=list(ai_rs),outfile=pr_odir,
+                                    fname=NA,writefile=F)
+          sim3d <- calc_similarity(par3d)
+          if (!inMemory(sim3d)) {sim3d <- readAll(sim3d)}
+          
+          #e. chirps cv
+          cat("   ...chirps c.v.\n")
+          par3e <- createParameters(x=in_data$Lon[i], y=in_data$Lat[i], vars=c("bio_1"),
+                                    weights=c(1),ndivisions=c(1),growing.season=NA,
+                                    rotation="none",threshold=1,env.data.ref=list(chcv_rs), 
+                                    env.data.targ=list(chcv_rs),outfile=pr_odir,
+                                    fname=NA,writefile=F)
+          sim3e <- calc_similarity(par3e)
+          if (!inMemory(sim3e)) {sim3d <- readAll(sim3e)}
+          
+          #calculate low limit
+          cat("   ...aggregating\n")
+          sim3 <- stack(c(sim3a, sim3b, sim3c, sim3d, sim3e)) %>% min(., na.rm=TRUE)
           if (!inMemory(sim3)) {sim3 <- readAll(sim3)}
 
           #4. combine all in a sensible layer and save individual layers and output as .RData
           #soil weight 50%, and climate indicators are divided equally over the remaining %
-          sim_i <- sim1 * 0.25 + sim2 * 0.5 + sim3 * 0.25
+          #sim_i <- sim1 * 0.25 + sim2 * 0.5 + sim3 * 0.25
+          cat("...minimum similarity across all factors\n")
+          sim_i <- stack(c(sim1, sim2, sim3)) %>%
+                      min(., na.rm=TRUE)
           if (!inMemory(sim_i)) {sim_i <- readAll(sim_i)}
 
           #5. put results in stack
@@ -228,47 +285,45 @@ run_points <- function(in_data, etype='pos') {
           if (!inMemory(simstk)) {simstk <- readAll(simstk)}
 
           #6. save results
+          cat("...saving\n")
           save(list=c("simstk"), 
                file=paste(pr_odir,"/out_",etype,"_",in_data$ID[i],".RData",sep=""),
                compress="xz",compression_level=9)
 
           #cleanup
-          rm(list=c("simstk","sim1","sim2","sim3a","sim3b","sim3","soil_data"))
+          cat("...clean-up\n")
+          rm(list=c("sim1","sim2","sim3a","sim3b","sim3c","sim3d","sim3e","sim3","soil_data"))
+          gc(full=TRUE, verbose=FALSE)
+      } else {
+          cat("...already processed, hence loading\n")
+          load(paste(pr_odir,"/out_",etype,"_",in_data$ID[i],".RData",sep=""))
       }
+      allstk <- c(allstk, simstk[["overall"]])
+      rm(simstk)
     }
     
-    #calculate maximum similarity across sites
-    if (!file.exists(paste(pr_odir,"/max_similarity_",etype,".tif",sep=""))) {
-        for (i in 1:nrow(in_data)) {
-            #i <- 1
-            cat("...processing site=",i,"\n")
-            load(paste(pr_odir,"/out_",etype,"_",in_data$ID[i],".RData",sep=""))
-            if (i == 1) {
-                maxsim <- simstk[["overall"]]
-            } else {
-                maxsim <- calc(stack(maxsim, simstk[["overall"]]), fun=max)
-            }
-            rm(simstk)
-        }
-
-        #write max similarity rasters
-        writeRaster(maxsim, paste(pr_odir,"/max_similarity_",etype,".tif",sep=""))
-    } else {
-        maxsim <- raster(paste(pr_odir,"/max_similarity_",etype,".tif",sep=""))
-    }
+    #maximum, minimum, and specific quantile values
+    maxsim <- max(stack(allstk), na.rm=TRUE) %>%
+                writeRaster(., paste(pr_odir,"/max_similarity_",etype,".tif",sep=""))
+    minsim <- min(stack(allstk), na.rm=TRUE) %>%
+                writeRaster(., paste(pr_odir,"/min_similarity_",etype,".tif",sep=""))
+    lowsim <- calc(stack(allstk), fun=function(x) {quantile(x, probs=0.2, na.rm=TRUE)}) %>%
+                writeRaster(., paste(pr_odir,"/lowlim_similarity_",etype,".tif",sep=""))
+    uppsim <- calc(stack(allstk), fun=function(x) {quantile(x, probs=0.8, na.rm=TRUE)}) %>%
+                writeRaster(., paste(pr_odir,"/upplim_similarity_",etype,".tif",sep=""))
     
     #cut similarity to growing areas
     #load harvested area
     aharv <- raster(paste(wd,"/spam2010V1r1/spam2010V1r1_global_H_MAIZ_A.tif",sep="")) %>%
                 raster::crop(., maxsim) %>%
                 raster::resample(., maxsim)
-    aharv[which(aharv[] < 0.01)] <- NA
+    aharv[which(aharv[] < 0.0001)] <- NA
     aharv[which(!is.na(aharv[]))] <- 1
 
     #mask
     if (!file.exists(paste(pr_odir,"/max_similarity_",etype,"_maizeareas.tif",sep=""))) {
-        maxsim_m <- raster::mask(maxsim, aharv)
-        writeRaster(maxsim_m, paste(pr_odir,"/max_similarity_",etype,"_maizeareas.tif",sep=""))
+        maxsim_m <- raster::mask(maxsim, aharv) %>%
+                    writeRaster(., paste(pr_odir,"/max_similarity_",etype,"_maizeareas.tif",sep=""))
     } else {
         maxsim_m <- raster(paste(pr_odir,"/max_similarity_",etype,"_maizeareas.tif",sep=""))
     }
@@ -315,44 +370,44 @@ ggsave(plot = g1, filename = paste(pr_odir,"/plot_similarity_overall.png",sep=""
 
 
 ####
-#run all negative points
-simres_n <- run_points(pdata_n, etype="neg")
-
-#pretty plot 
-#first mask to Africa shapefile
-rsp <- raster::mask(simres_n[[2]], sh_ctry)
-
-#to points (for use of geom_tile)
-rs_vls <- rasterToPoints(rsp) %>% 
-    as_tibble() %>% 
-    setNames(c('x', 'y', 'value'))
-
-#plot
-g1 <- ggplot(rs_vls)  +
-    geom_tile(aes(x = x, y =  y, fill = value)) +
-    scale_fill_divergent(low='tomato', 
-                         mid = 'lemonchiffon', 
-                         high = 'darkcyan', 
-                         midpoint=0.5, 
-                         na.value="white",
-                         limits=c(0,1)) +
-    coord_fixed(ratio=1,xlim = c(-20,60), ylim = c(-40,40)) +
-    #coord_equal() +
-    geom_polygon(data=sh_ctry, 
-                aes(x=long, y=lat, group=group), 
-                color="grey20", size=0.25, fill=NA) +
-    theme_bw() +
-    labs(x = 'Longitude', y = 'Latitude', fill = "Similarity", caption = 'Smallholder Adaptation Atlas') +
-    theme(panel.grid.major=element_blank(),
-          panel.grid.minor=element_blank(),
-          plot.background=element_blank(),
-          strip.background = element_rect(color="white", fill="white"),
-          strip.text = element_text(size=12, face = "bold"))
-ggsave(plot = g1, filename = paste(pr_odir,"/plot_neg_similarity_overall.png",sep=""), units = 'in', width = 9, height = 9, dpi = 300)
-
-#simple plots
-plot(simres_p[[2]],zlim=c(0,1),breaks=seq(0,1,by=0.25),col=rev(terrain.colors(4)))
-plot(sh_ctry, add=T)
+##run all negative points
+#simres_n <- run_points(pdata_n, etype="neg")
+#
+##pretty plot 
+##first mask to Africa shapefile
+#rsp <- raster::mask(simres_n[[2]], sh_ctry)
+#
+##to points (for use of geom_tile)
+#rs_vls <- rasterToPoints(rsp) %>% 
+#    as_tibble() %>% 
+#    setNames(c('x', 'y', 'value'))
+#
+##plot
+#g1 <- ggplot(rs_vls)  +
+#    geom_tile(aes(x = x, y =  y, fill = value)) +
+#    scale_fill_divergent(low='tomato', 
+#                         mid = 'lemonchiffon', 
+#                         high = 'darkcyan', 
+#                         midpoint=0.5, 
+#                         na.value="white",
+#                         limits=c(0,1)) +
+#    coord_fixed(ratio=1,xlim = c(-20,60), ylim = c(-40,40)) +
+#    #coord_equal() +
+#    geom_polygon(data=sh_ctry, 
+#                aes(x=long, y=lat, group=group), 
+#                color="grey20", size=0.25, fill=NA) +
+#    theme_bw() +
+#    labs(x = 'Longitude', y = 'Latitude', fill = "Similarity", caption = 'Smallholder Adaptation Atlas') +
+#    theme(panel.grid.major=element_blank(),
+#          panel.grid.minor=element_blank(),
+#          plot.background=element_blank(),
+#          strip.background = element_rect(color="white", fill="white"),
+#          strip.text = element_text(size=12, face = "bold"))
+#ggsave(plot = g1, filename = paste(pr_odir,"/plot_neg_similarity_overall.png",sep=""), units = 'in', width = 9, height = 9, dpi = 300)
+#
+##simple plots
+#plot(simres_p[[2]],zlim=c(0,1),breaks=seq(0,1,by=0.25),col=rev(terrain.colors(4)))
+#plot(sh_ctry, add=T)
 
 ########################################################################
 ########################################################################
